504f1a6a0334e3939a8fa910b43b8223b636544a
Gilles ZAOUI <gizaoui@gmail.com>
2024-04-16 17:43:56 +0200

    .

--- Kubernetes/ngnix/index.php
+++ Kubernetes/ngnix/index.php
@@ -1,8 +1,3 @@
-<html>
-    <head>
-        <title>PHP Hello World!</title>
-    </head>
-    <body>
-        <?php echo '<h1>This is My first php page</h1>'; ?>
-    </body>
-</html>
+<?php
+phpinfo();
+?>
